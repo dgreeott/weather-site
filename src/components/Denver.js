013fa8c89bcc588a7ba8f55a1b490005c2a89f82
dgreeott <48890665+dgreeott@users.noreply.github.com>
2021-01-29 16:07:31 -0700

    Trying to Create A Secure API Key

--- src/components/Denver.js
+++ src/components/Denver.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import '../css/App.css';
+import "../css/App.css";
+
+const API_Key = process.env.REACT_APP_WEATHER_API_KEY;
 
 export default class Denver extends Component {
   state = {
@@ -8,33 +10,39 @@ export default class Denver extends Component {
   };
 
   async componentDidMount() {
-    const url =
-      "https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=9988e46978e6f97c7ef21c5e1ed201da&units=imperial";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=${API_Key}`;
     const response = await fetch(url);
     const data = await response.json();
     this.setState({ city: data, loading: false });
+    console.log(data);
   }
 
   render() {
     return (
       <>
-      <div className="container-fluid">
-        {this.state.loading || !this.state.city ? (
-          <div>Loading...</div>
-        ) : (
-          <div className="containter">
-            <div>
-              <h1>{this.state.city.name}</h1>
+        <div className="container-fluid">
+          {this.state.loading || !this.state.city ? (
+            <div>Loading...</div>
+          ) : (
+            <div className="containter">
+              <div>
+                <h1>{this.state.city.name}</h1>
+              </div>
+              <div>
+                <h2>Temp: {this.state.city.main.temp}&#176;</h2>
+              </div>
+              <div>
+                <div>
+                  <h3>
+                    Min: {this.state.city.main.temp_min}&#176; | Min:{" "}
+                    {this.state.city.main.temp_max}&#176;
+                  </h3>
+                </div>
+              </div>
             </div>
-            <div>
-              <h2>Temp:{" "}{this.state.city.main.temp}&#8457;</h2>
-            </div>
-            <div>
-              <div><h3>Min:{" "}{this.state.city.main.temp_min}&#8457; | Min:{" "}{this.state.city.main.temp_max}&#8457;</h3></div>
-            </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
       </>
     );
   }
