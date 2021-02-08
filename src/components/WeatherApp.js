92d27800c65504fde50b241e2267711bef488752
dgreeott <48890665+dgreeott@users.noreply.github.com>
2021-02-08 14:04:52 -0700

    Got the daily forecast data to display on the screen

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -7,7 +7,7 @@ import "../css/App.css";
 const WeatherApp = () => {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
-  const [forcast, setForcsat] = useState({});
+  const [forecast, setForecast] = useState({});
 
   const searchWeather = async (e) => {
     if (e.key === "Enter") {
@@ -15,7 +15,13 @@ const WeatherApp = () => {
 
       setWeather(dataWeather);
       setQuery("");
-    }
+
+      const lat = dataWeather.coord.lat;
+      const lon = dataWeather.coord.lon;
+
+      const dataForecast = await fetchForecast(lat, lon);
+      setForecast(dataForecast);
+    } 
   };
 
   return (
@@ -26,7 +32,7 @@ const WeatherApp = () => {
         placeholder="Search..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        onKeyPress={searchWeather}
+        onKeyPress={(searchWeather)}
       />
       {weather.main && (
         <div className="city">
@@ -44,7 +50,25 @@ const WeatherApp = () => {
               alt={weather.weather[0].description}
             />
             <p>{weather.weather[0].description}</p>
-            <button className="btn btn-primary">4 Day Forcast</button>
+          </div>
+        </div>
+      )}
+      {forecast.daily && (
+        <div className="city m-5">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+          </h2>
+          <div className="city-temp">
+          {Math.round(forecast.daily[0].temp.day)}
+            <sup>&deg;</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
           </div>
         </div>
       )}
