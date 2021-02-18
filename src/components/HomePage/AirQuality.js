8c59d6e97d417c19f20623f0a4e2204985a56427
dgreeott <48890665+dgreeott@users.noreply.github.com>
2021-02-18 16:18:34 -0700

    Updated Styling

--- src/components/HomePage/AirQuality.js
+++ src/components/HomePage/AirQuality.js
@@ -1,7 +1,25 @@
 import React from "react";
 /*import moment from "moment";*/
 
-const airQuality = (props) => {
+const AirQuality = (props) => {
+  const [rating, setRating] = useState({});
+
+  const qualityRating = (rating) => {
+    if (props.airQuality.list[0].main.aqi === 1){
+      rating = "Good";
+    } else if(props.airQuality.list[0].main.aqi === 2)
+    {
+      rating ="Fair";
+    }
+    else if(props.airQuality.list[0].main.aqi === 3)
+    {
+      rating ="Poor";
+    }else if(props.airQuality.list[0].main.aqi === 4)
+    {
+      rating =" Very Poor";
+    }
+  }
+
   return (
     <>
       {props.airQuality.list && (
@@ -9,13 +27,14 @@ const airQuality = (props) => {
           <div className="row covidInfo">
             <div className="col-sm">
               <div className="row text-center">
-                <div className="col-sm">
-                  <h4>Air Quality</h4>
+                <div className="col-sm mb-3">
+                  <h3>Air Quality</h3>
                 </div>
               </div>
               <div className="row">
-                <div className="col-sm">
+                <div className="col-sm text-center">
                   <h2>{props.airQuality.list[0].main.aqi}</h2>
+                  <p>{qualityRating(rating)}</p>
                 </div>
               </div>
             </div>
@@ -26,4 +45,4 @@ const airQuality = (props) => {
   );
 };
 
-export default airQuality;
+export default AirQuality;
